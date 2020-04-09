7333bf5c74de24349dc1ab3314f6937a30a26617
Omar Flores Grimontt <omar.flores@3dves.com>
2020-04-09 08:34:37 -0500

    fixed image thing

--- index.js
+++ index.js
@@ -17,11 +17,14 @@ function dropHandler(event) {
 function addImage () {
     const container = document.getElementById('left')
     const imgs = document.querySelectorAll('img')
+    const imgContainer = document.createElement('div')
     const img = document.createElement('img')
+    imgContainer.className = 'img-container'
+    imgContainer.id = `i${imgs.length + 1}`
+    imgContainer.draggable = true
+    imgContainer.addEventListener('dragstart', dragstartHandler)
     img.className = 'img'
-    img.id = `i${imgs.length + 1}`
-    img.draggable = true
-    img.addEventListener('dragstart', dragstartHandler)
-    img.src = `https://picsum.photos/150/150?grayscale?${img.id}`
-    container.appendChild(img)
+    img.src = `https://picsum.photos/150/150?grayscale?${imgContainer.id}`
+    imgContainer.appendChild(img)
+    container.appendChild(imgContainer)
 }
