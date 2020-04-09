4f5bbd3d88f65bc4925ab64c5973342869d4f4b8
Omar Flores Grimontt <omar.flores@3dves.com>
2020-04-09 08:14:10 -0500

    removed grayscale

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function addImage () {
     const img = document.createElement('img')
     img.className = 'img'
     img.id = `i${imgs.length + 1}`
-    img.src = `https://picsum.photos/150/150?grayscale?${img.id}`
+    img.src = `https://picsum.photos/150/150?${img.id}`
     container.appendChild(img)
 }
