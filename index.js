54a65b1854d354e962fad1534185430d347f95bf
Omar Flores Grimontt <omar.flores@3dves.com>
2020-04-09 08:37:38 -0500

    presentation initial state

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ function addImage () {
     const img = document.createElement('img')
     imgContainer.className = 'img-container'
     imgContainer.id = `i${imgs.length + 1}`
-    imgContainer.draggable = true
-    imgContainer.addEventListener('dragstart', dragstartHandler)
     img.className = 'img'
     img.src = `https://picsum.photos/150/150?grayscale?${imgContainer.id}`
     imgContainer.appendChild(img)
